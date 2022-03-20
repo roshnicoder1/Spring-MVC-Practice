51c98ec5842ee2a3e11e5c33e22edbaf76ca502e
Roshni Kumari011 <roshnkum@in.ibm.com>
2022-03-20 14:37:08 +0530

    first-commit

---
+++ src/main/java/com/ibm/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.ibm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+ 
+	@RequestMapping("/hello")  
+	public String display()  
+	{  
+	    return "hello";  
+	}  
+	@RequestMapping(value="/helloagain",method=RequestMethod.GET)  
+	public String displays()  
+	{  
+	    return "final";  
+	} 
+
+}
